d61526909321336692c873fa483fb7beb20920c1
anfaas1618 <ishad0911@gmail.com>
2022-09-11 10:05:23 +0530

    final Commit

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
-{
+{int mask = 1 << 10;
     Vector3 _vec;
     public float speed =23;
+    public LayerMask layers;
+    public float rayDistance;
+    private bool grounded;
 
     public GameObject canvas;
     // Start is called before the first frame update
@@ -15,13 +18,21 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()  
-    {  
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
         
-        _vec = transform.localPosition;
-        _vec.x += Input.GetAxis("Horizontal") * Time.deltaTime * 8*speed;  
-        _vec.z += Input.GetAxis("Vertical") * Time.deltaTime * 8*speed;  
-        transform.localPosition = _vec;
+        grounded = Physics.Raycast(ray, out hit, rayDistance, layers);
+        if (grounded)
+        {
+            _vec = transform.localPosition;
+            _vec.x += Input.GetAxis("Horizontal") * Time.deltaTime * 8 * speed;
+            _vec.z += Input.GetAxis("Vertical") * Time.deltaTime * 8 * speed;
+            transform.localPosition = _vec;
+
+        }
+
         if (transform.position.y < -10)
         {    
             canvas.SetActive(true);
